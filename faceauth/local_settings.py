73016910f20d7a4d12ad0f528827e240ab730865
Varnan <varnan.k@rayiot.org>
2019-05-28 17:10:21 +0530

    Credentails removed

--- faceauth/local_settings.py
+++ faceauth/local_settings.py
@@ -6,8 +6,8 @@ LOGOUT_URL = 'logout'
 LOGOUT_REDIRECT_URL = 'login'
 
 
-SOCIAL_AUTH_FACEBOOK_KEY = "780160385719192"          # App ID
-SOCIAL_AUTH_FACEBOOK_SECRET = "96309406eaf2e99df01dbab1009091be"       # App Secret
+SOCIAL_AUTH_FACEBOOK_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"          # App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"       # App Secret
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_link'] # add this
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {       # add this
                                       'fields': 'id, name, email, picture.type(large), link'
